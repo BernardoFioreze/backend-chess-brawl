9694148667137d6e64ec89467fcdf87c351ab167
Bernardo Fioreze <bernardofioreze@gmail.com>
2025-05-01 14:24:41 -0300

    First Commit

---
+++ src/main/java/com/backend/backend_chess_brawl/BackendChessBrawlApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backend_chess_brawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChessBrawlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChessBrawlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/backend_chess_brawl/BackendChessBrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backend_chess_brawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChessBrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
