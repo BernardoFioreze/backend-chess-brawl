f831c7890293f2025c899427ed08ba7a97c3c341
Bernardo Fioreze <bernardofioreze@gmail.com>
2025-05-01 15:52:59 -0300

    Model Match

---
+++ src/main/java/com/backend/backend_chess_brawl/model/Match.java
@@ -0,0 +1,41 @@
+package com.backend.backend_chess_brawl.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "match")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player1_id")
+    private Player player1;
+
+    @ManyToOne
+    @JoinColumn(name = "player2_id")
+    private Player player2;
+
+    @Column(name = "isFinished")
+    private boolean isFinished;
+
+    @ManyToOne
+    @JoinColumn(name = "round_id")
+    private Round round;
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id")
+    private Tournament tournament;
+
+}
