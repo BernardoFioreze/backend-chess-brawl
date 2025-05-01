1cd31a72501b9772a020aa9eaa31d68efefbc6f4
Bernardo Fioreze <bernardofioreze@gmail.com>
2025-05-01 15:53:19 -0300

    Model Tournament

---
+++ src/main/java/com/backend/backend_chess_brawl/model/Tournament.java
@@ -0,0 +1,37 @@
+package com.backend.backend_chess_brawl.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tournament")
+public class Tournament {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "tournament", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Player> players;
+    
+    @OneToMany(mappedBy = "tournament", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Match> matches;
+    
+    @OneToMany(mappedBy = "tournament", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Round> rounds;
+    
+
+    @Column(name = "winner")
+    private String winner;
+
+}
