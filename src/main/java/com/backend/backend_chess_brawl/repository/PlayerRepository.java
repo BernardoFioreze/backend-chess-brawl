50c3a46d9b0042ad680e66d463f670f27a1e2de0
Bernardo Fioreze <bernardofioreze@gmail.com>
2025-05-01 16:28:49 -0300

    Repository

---
+++ src/main/java/com/backend/backend_chess_brawl/repository/PlayerRepository.java
@@ -0,0 +1,13 @@
+package com.backend.backend_chess_brawl.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.backend_chess_brawl.model.Player;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    Optional<Player> findByName(String name);
+
+}
