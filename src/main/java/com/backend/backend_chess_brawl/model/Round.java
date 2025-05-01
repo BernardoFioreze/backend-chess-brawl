59cce78f2ad9002f0af929d29f53ca87425ef1e2
Bernardo Fioreze <bernardofioreze@gmail.com>
2025-05-01 15:53:10 -0300

    Model Round

---
+++ src/main/java/com/backend/backend_chess_brawl/model/Round.java
@@ -0,0 +1,36 @@
+package com.backend.backend_chess_brawl.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "round")
+public class Round {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "isFinished")
+    private boolean isFinished;
+
+    @OneToMany(mappedBy = "round", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Match> matches;
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id")
+    private Tournament tournament;
+
+}
